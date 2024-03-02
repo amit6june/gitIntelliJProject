122df13c5edfddaab7583964f1113c4f2add67c9
amit6june <kumar.amit.mishra@gmail.com>
2024-03-03 08:36:13 +1100

    first commit

--- src/main/java/org/example/newController.java
+++ src/main/java/org/example/newController.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class newController {
+
+    public void add(){
+
+        System.out.print("Hello world! 1234 ");
+
+        System.out.print("Hello world! 12345678 ");
+        System.out.print("Hello world! 12349876 ");
+
+
+
+    }
 }
